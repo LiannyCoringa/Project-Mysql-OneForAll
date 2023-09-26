01708b7dc62fc42e02a306f49dcfb30fb146bef0
Lianny Coringa <lianny_costa@hotmail.com>
2023-09-26 12:14:47 -0300

    Alterando o 10

--- challenges/desafio10.sql
+++ challenges/desafio10.sql
@@ -1,21 +1,23 @@
 CREATE TABLE SpotifyClone.favoritos(
 	cancao_id INT NOT NULL,
     usuario_id INT NOT NULL,
-    CONSTRAINT PRIMARY KEY(cancao_id, usuario_id),
+    album_id INT NOT NULL,
+    CONSTRAINT PRIMARY KEY(cancao_id, usuario_id, album_id),
     FOREIGN KEY (cancao_id) REFERENCES cancao (cancao_id),
-    FOREIGN KEY (usuario_id) REFERENCES usuario (usuario_id)
+    FOREIGN KEY (usuario_id) REFERENCES usuario (usuario_id),
+    FOREIGN KEY (album_id) REFERENCES albuns (album_id)
 ) ENGINE = InnoDB;
-INSERT INTO SpotifyClone.favoritos(cancao_id, usuario_id)
-VALUES (3, 1),
-(6, 1),
-(10, 1),
-(4, 2),
-(1, 3),
-(3, 3),
-(7, 4),
-(4, 4),
-(10, 5),
-(2, 5),
-(4, 8),
-(7, 9),
-(3, 10);
+INSERT INTO SpotifyClone.favoritos(cancao_id, usuario_id, album_id)
+VALUES (3, 1, 1),
+(6, 1, 4),
+(10, 1, 8),
+(4, 2, 2),
+(1, 3, 1),
+(3, 3, 1),
+(7, 4, 5),
+(4, 4, 2),
+(10, 5, 8),
+(2, 5, 1),
+(4, 8, 2),
+(7, 9, 5),
+(3, 10, 1);
